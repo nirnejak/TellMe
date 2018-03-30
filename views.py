2a6e61ca672ee202586acd52dc3483d4376e5a67
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2018-03-30 23:54:03 +0530

    Message Broadcast - Form

--- views.py
+++ views.py
@@ -152,10 +152,61 @@ def user():
 @app.route('/message', methods=['GET', 'POST'])
 def message():
 	if request.method == 'POST':
-		pass
-	data = {
+		state = form.data["state"]
+		district = form.data["district"]
+		crop = form.data["crop"]
+		irrigationSource = form.data["irrigationSource"]
+		messageBody = form.data["messageBody"]
+		messageLink = form.data["messageLink"]
+		messageType = form.data["messageType"]
+	else:
+		# Connecting to database
+		conn = pg2.connect(database="db70oouohkh4bj",user="fnqryfoivwpuxd",password="884e1a40af2227d023c58401914873dfea4d44530a34647bb5930872b1a21807",host="ec2-54-221-220-59.compute-1.amazonaws.com",port="5432")
+
+		try:
+			# Creating cursor
+			cur = conn.cursor()
+
+            # Executing Query
+			cur.execute("SELECT DISTINCT district FROM location_dim ORDER BY district;")
+
+			# Fetching Data
+			districtData = cur.fetchall()
+
+			# Executing Query
+			cur.execute("SELECT DISTINCT crop_name FROM crop_dim ORDER BY crop_name;")
+
+			# Fetching Data
+			cropData = cur.fetchall()
+
+			# Executing Query
+			cur.execute("SELECT DISTINCT water_source FROM fact_table ORDER BY water_source;")
+
+			# Fetching Data
+			sourceData = cur.fetchall()
+		except:
+			conn.rollback()
+			res = {
+                "status" : "failed",
+                "message" : "Something went wrong",
+                "error" : ""
+            }
+			return jsonify(res)
+
+        # Commiting the Changes
+		conn.commit()
+
+        # Closing the cursor
+		cur.close()
+
+		# Closing the connection
+		conn.close()
+		
+		data = {
 			"stateData" : stateData,
-			"district" : "district"
+			"districtData" : districtData,
+			"cropData" : cropData,
+			"sourceData" : sourceData
 		}
 	return render_template('message.html', data = data)
 
