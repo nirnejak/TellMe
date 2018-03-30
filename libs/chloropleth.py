1d30bfc1604c2394441f1d579025bc0ea54971f4
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2018-03-31 04:42:02 +0530

    Bug Fix - Chloropleth

--- libs/chloropleth.py
+++ libs/chloropleth.py
@@ -1,10 +1,12 @@
-def chloropleth(date_from, date_to):
+import datetime as dt
+
+def chloropleth(date_from = dt.date(2000,1,1), date_to = dt.date.today()):
     import pandas as pd
     import sqlalchemy as sql
     conn = sql.create_engine('postgresql://fnqryfoivwpuxd:884e1a40af2227d023c58401914873dfea4d44530a34647bb5930872b1a21807@ec2-54-221-220-59.compute-1.amazonaws.com/db70oouohkh4bj')
-    avg_color_values = pd.read_sql('''select sum(water_amount) as sum, count(water_amount) as count, state from location_dim, fact_table 
-                                    where location_dim.farm_id=fact_table.location_id and irrigation_date >= \''''+
-                                  str(date_from)+'\''+' and irrigation_date <= '+'\''+str(date_to)+'\''+' group by state;', conn)
+    avg_color_values = pd.read_sql('''SELECT SUM(water_amount) as sum, COUNT(water_amount) AS count, state FROM location_dim, fact_table 
+                                    WHERE location_dim.farm_id = fact_table.location_id AND irrigation_date >= \''''+
+                                  str(date_from)+'\''+' AND irrigation_date <= '+'\''+str(date_to)+'\''+' GROUP BY state;', conn)
     avg_color_values['avg'] = avg_color_values['sum']/avg_color_values['count']
     avg_color_values.set_index('state', inplace=True)
     ret = {}
